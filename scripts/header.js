684e7bb999aa45fa591faf1e47c7e394ef3c2510
toriisimone <toriisimone@gmail.com>
2025-12-31 12:08:42 -0500

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,1 +1,13 @@
-placeholder.txt
+fetch("header.html")
+  .then(res => res.text())
+  .then(html => {
+    // Insert header at the very top of the body
+    document.body.insertAdjacentHTML("afterbegin", html);
+
+    // Load header stylesheet
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "styles/header.css";
+    document.head.appendChild(link);
+  })
+  .catch(err => console.error("Header failed to load:", err));
