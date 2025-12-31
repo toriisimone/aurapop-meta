a23313667cdad89e27a0678238ebf87c07c0ac39
toriisimone <toriisimone@gmail.com>
2025-12-31 12:05:07 -0500

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,10 +1,13 @@
 fetch("footer.html")
   .then(res => res.text())
   .then(html => {
+    // Insert footer at the end of the body
     document.body.insertAdjacentHTML("beforeend", html);
 
+    // Load footer stylesheet
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = "styles/footer.css";
     document.head.appendChild(link);
-  });
+  })
+  .catch(err => console.error("Footer failed to load:", err));
